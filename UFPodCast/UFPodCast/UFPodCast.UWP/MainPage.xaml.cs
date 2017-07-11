76895304bf42f99a78436d1dccdfd890e3cfab39
danieltabares <danitabares5@hotmail.com>
2017-07-10 20:14:04 -0500

    create first version proyect

---
+++ UFPodCast/UFPodCast/UFPodCast.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace UFPodCast.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new UFPodCast.App());
+        }
+    }
+}
---
+++ UFPodCast/UFPodCast/UFPodCast/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace UFPodCast.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
