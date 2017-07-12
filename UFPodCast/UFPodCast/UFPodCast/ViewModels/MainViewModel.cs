47afcc63603dc4d13a7e3d4a6d1c31ff57e76fd9
danieltabares <danitabares5@hotmail.com>
2017-07-12 14:51:04 -0500

    update adding new arquitecture

---
+++ UFPodCast/UFPodCast/UFPodCast/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.Collections.Generic;
+using UFPodCast.ViewModels;
+using System.ComponentModel;
+using UFPodCast.Models;
+using UFPodCast.Services;
+
+namespace UFPodCast.ViewModels
+{
+    public class MainViewModel //: INotifyPropertyChanged
+    {
+        #region Attributes
+        private ApiService apiService;
+        private DataService dataService;
+        private DialogService dialogService;
+        private NavigationService navigationService;
+        private bool isRefreshing = false;
+        //private User currentUser;
+        #endregion
+
+       
+
+        #region Constructor
+        public MainViewModel()
+        {
+          
+        }
+        #endregion
+
+      
+    }
+
+}
